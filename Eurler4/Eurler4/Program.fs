f63f9cac28beef0fea75ede0274341bb8ef46f66
Darius Baba <babad@kno.com>
2013-01-12 18:29:12 +0200

    Project Euler problem 4

---
+++ Eurler4/Eurler4/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+let IsPalindrome n = 
+       let inv =  n.ToString().ToCharArray() |> Array.rev
+       n.ToString().Equals(new string(inv))
+
+let MapToFactors n = 
+       let div = seq {999 .. -1 .. 100} |> Seq.tryFind (fun x -> (n % x = 0) && ( n / x).ToString().Length = 3) 
+       match div with
+       | Some(i) -> (n/Some(i).Value, Some(i).Value)
+       | None -> (0,0)
+
+let palindromes = seq {999999 .. -1 .. 10000} 
+                  |> Seq.filter IsPalindrome 
+                  |> Seq.map MapToFactors 
+                  |> Seq.find (fun x -> fst(x) <> 0) 
+                  |> (fun x -> (fst(x)*snd(x), x))
+
+
+
