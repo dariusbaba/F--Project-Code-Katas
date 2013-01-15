c477da91bf9e9dac827237a1f847c9257e2374e9
darius <darius@darius-Inspiron-N5010.(none)>
2013-01-15 22:24:13 +0200

    First commit for EulerPb5

---
+++ Euler5/Euler5/Component1.fs
@@ -0,0 +1,11 @@
+
+// NOTE: If warnings appear, you may need to retarget this project to .NET 4.0. Show the Solution
+// Pad, right-click on the project node, choose 'Options --> Build --> General' and change the target
+// framework to .NET 4.0 or .NET 4.5.
+
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+let rec getPrimes n =
+    
